ab2cdda14a64a863582547ffafcbe1458d02e61f
Quintin <quinndonnelly22@gmail.com>
2016-12-11 05:37:21 -0600

    Added the check to ensure that the path is not null and is less than 16 charecters

--- main.cpp
+++ main.cpp
@@ -4,6 +4,30 @@
 
 using namespace std;
 
+bool checkPath(char *path)
+{
+	char * it = path;
+	int length = 0;
+	while (*it != '\0')
+	{
+		++length;
+		++it;
+	}
+
+	if (length == 0)
+	{
+		cout << "Error empty path given\n";
+		exit(1);
+	}
+	else if (length > 15)
+	{
+		cout << "Error path must be less than 16 charecters.\n";
+		exit(1);
+	}
+
+	return true;
+}
+
 void usage(char *prog)
 {
 	fprintf(stderr, "usage: %s <disk image file>\n", prog);
@@ -17,8 +41,7 @@ main(int argc, char *argv[])
 		usage(argv[0]);
 	}
 	char *path = argv[1];
-
-	cout << "\"" << argv[1] << "\"";
+	checkPath(path);
 
 	FS_Boot(path);
 	FS_Sync();
